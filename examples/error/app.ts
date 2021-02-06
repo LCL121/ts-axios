8dd7d5fd2cf4778954b77638c03fdb15735fe9d3
LCL121 <1391436522@qq.com>
2021-02-06 15:47:28 +0800

    test(expamples): 增加测试异常错误功能的例子
    增加测试异常错误功能的例子

---
+++ examples/error/app.ts
@@ -0,0 +1,44 @@
+import axios, { AxiosError } from '../../src/index'
+
+axios({
+  url: '/error/get1',
+  method: 'get'
+}).then(res => {
+  console.log(res)
+}).catch(err => {
+  console.log(err)
+})
+
+axios({
+  url: '/error/get',
+  method: 'get'
+}).then(res => {
+  console.log(res)
+}).catch(err => {
+  console.log(err)
+})
+
+setTimeout(() => {
+  axios({
+    url: '/error/get',
+    method: 'get'
+  }).then(res => {
+    console.log(res)
+  }).catch(err => {
+    console.log(err)
+  })
+}, 5000)
+
+axios({
+  url: '/error/timeout',
+  method: 'get',
+  timeout: 2000
+}).then(res => {
+  console.log(res)
+}).catch((err: AxiosError) => {
+  console.log(err.message)
+  console.log(err.config)
+  console.log(err.code)
+  console.log(err.request)
+  console.log(err.isAxiosError)
+})
