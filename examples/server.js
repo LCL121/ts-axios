d20aa5851c0d5eff5e7033c1dc44043a9ae71d73
LCL121 <1391436522@qq.com>
2021-02-08 10:45:13 +0800

    test(examples): 增加测试跨域的例子
    增加测试跨域的例子

--- examples/server.js
+++ examples/server.js
@@ -1,9 +1,12 @@
 const express = require('express')
 const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const webpack = require('webpack')
 const webpackDevMiddleware = require('webpack-dev-middleware')
 const webpackHotMiddleware = require('webpack-hot-middleware')
 const WebpackConfig = require('./webpack.config')
+
+require('./server2')
 
 const app = express()
 const complier = webpack(WebpackConfig)
@@ -22,6 +25,7 @@ app.use(express.static(__dirname))
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(cookieParser())
 
 const router = express.Router()
 
@@ -32,6 +36,7 @@ registerExtendRouter()
 registerInterceptorRrouter()
 registerConfigRouter()
 registerCancelRouter()
+registerMoreRouter()
 
 app.use(router)
 
@@ -159,5 +164,37 @@ function registerCancelRouter() {
     setTimeout(() => {
       res.json(req.body)
     }, 1000)
+  })
+}
+
+function registerMoreRouter() {
+  router.get('/more/get', (req, res) => {
+    res.json(req.cookies)
+  })
+
+  router.post('/more/post', function (req, res) {
+    const auth = req.headers.authorization
+    const [type, credentials] = auth.split(' ')
+    console.log('atob on server:', atob(credentials))
+    const [username, password] = atob(credentials).split(':').map(item => item.trim())
+    if (type === 'Basic' && username === 'chen' && password === '123456') {
+      res.json(req.body)
+    } else {
+      res.status(401)
+      res.end('UnAuthorization')
+    }
+  })
+
+  router.get('/more/304', function (req, res) {
+    res.status(304)
+    res.end()
+  })
+
+  router.get('/more/A', function (req, res) {
+    res.end('A')
+  })
+
+  router.get('/more/B', function (req, res) {
+    res.end('B')
   })
 }
